f28de0b012b049c1b87e585c9f1ee4e38296fd29
Edith02 <edithbalole@gmail.com>
2022-08-03 01:51:47 +0400

    Initial commit

---
+++ app/src/main/java/com/example/hot_food_delivery_pmo/Activity/CartActivity.java
@@ -0,0 +1,71 @@
+package com.example.hot_food_delivery_pmo.Activity;
+
+import android.os.Bundle;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.hot_food_delivery_pmo.Adapter.CartListAdapter;
+import com.example.hot_food_delivery_pmo.Helper.ManagementCart;
+import com.example.hot_food_delivery_pmo.Interface.ChangeNumberItemsListener;
+import com.example.hot_food_delivery_pmo.R;
+
+public class CartActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerViewList;
+    private ManagementCart managementCart;
+    private TextView totalFeeTxt, taxTxt, deliveryTxt, totalTxt, emptyTxt;
+    private double tax;
+    private ScrollView scrollView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+
+        managementCart = new ManagementCart(this);
+
+        initView();
+        initList();
+    }
+    private void initList() {
+        linearLayoutManager linearLayoutManager = new linearLayoutManager(this, linearLayoutManager.VERTICAL, false);
+        recyclerViewList.setLayoutManager(linearLayoutManager);
+        adapter = new CartListAdapter(managementCart.getListCart(), this, new ChangeNumberItemsListener() {
+            @Override
+            public void changed() {
+                calculateCard();
+            }
+        });
+    }
+
+    recyclerViewList.setAdapter(adapter);
+
+
+    private void calculateCard() {
+        double percentTax = 0.05;
+        double delivery = 10;
+
+        tax = Math.round((managementCart.getTotalFee() * percentTax) * 100.0) / 100.0;
+        double total = Math.round((managementCart.getTotalFee() + tax + delivery) * 100.0)/ 100.0;
+        double itemTotal = Math.round(managementCart.getTotalFee() * 100.0)/ 100.0;
+
+        totalFeeTxt.setText("$"+itemTotal);
+        taxTxt.setText("$"+tax);
+        deliveryTxt.setText("$"+delivery);
+        totalTxt.setText("$"+totalTxt);
+
+    }
+
+    private void initView() {
+        totalFeeTxt=findViewById(R.id.totalFeeTxt);
+        taxTxt=findViewById(R.id.taxTxt);
+        deliveryTxt=findViewById(R.id.deliveryTxt);
+        totalTxt=findViewById(R.id.totalTxt);
+        recyclerViewList=findViewById(R.id.view);
+        scrollView=findViewById(R.id.scrollView3);
+        emptyTxt=findViewById(R.id.emptyTxt);
+    }
+}
---
+++ app/src/main/java/com/example/hot_food_delivery_pmo/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package com.example.hot_food_delivery_pmo.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
